13b95a210cd756d047954e942a734ee9182bbae1
Himanshu Vaishya <himanshuv@argsupplytech.com>
2024-08-01 19:12:33 +0530

    change in database.js for connection

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const dbConnect = () => {
   mongoose
-    .connect(process.env.DATABASE_URL)
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("DB connection is successful"))
     .catch((error) => {
       console.log("Issue in Db connection");
@@ -16,65 +16,4 @@ const dbConnect = () => {
 };
 
 module.exports = dbConnect;
-
-
-// var mysql=require('mysql');
-
-// const dbConnect =()=>{
-
-// var connection=mysql.createConnection({
-//   host:"supplier123.database.windows.net",
-//   user:"supplier123",
-//   password:"ARG@supply123",
-//   database:"supplier",
-//   // ssl: {
-//   //   ca: sslCert
-//   // },
-//   connectTimeout: 10000 
-// });
-
-// connection.connect(function(err){
-//   if(err){
-//     console.log(err.stack)
-//     throw err};
-//   console.log("connected");
-// })
-
-// return connection;
-
-// }
-
-
-// module.exports=dbConnect;
-
-
-// const mysql = require('mysql');
-// const fs = require('fs');
-// const path = require('path');
-
-
-// const dbConnect = () => {
-//   var connection = mysql.createConnection({
-//     host: "supplier123.database.windows.net",
-//     user: "supplier123",
-//     password: "ARG@supply123",
-//     database: "supply",
-//     // ssl: {
-//     //   ca: sslCert
-//     // },
-//     connectTimeout: 10000
-//   });
-
-//   connection.connect(function (err) {
-//     if (err) {
-//       console.log('Error connecting to the database:', err.stack);
-//       return;
-//     }
-//     console.log("Connected to the database");
-//   });
-
-//   return connection;
-// };
-
-// module.exports = dbConnect;
 
